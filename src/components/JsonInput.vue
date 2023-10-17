bbb69cb42f21bc0dacaff883f15008d6ad73d746
Luis von Rondow <luiseduardorondow@gmail.com>
2023-10-17 11:20:27 +0100

    feat: virtual list

--- src/components/JsonInput.vue
+++ src/components/JsonInput.vue
@@ -7,34 +7,42 @@ const { handle } = defineProps({
 })
 
 const handleFileChange = (e) => {
-  const file = e.target.files[0];
-  if (!file) return;
+  const file = e.target.files[0]
+  if (!file) return
 
-  // Check if the file has a .json extension
   if (file.name.slice(-5).toLowerCase() !== '.json') {
-    alert('Please upload a valid JSON file.');
-    return;
+    alert('Please upload a valid JSON file.')
+    return
   }
 
-  const reader = new FileReader();
-  reader.readAsText(file, 'UTF-8');
-  reader.onload = (e) => {
-    try {
-      console.log('Loaded JSON file with size: ', e.total);
-      handle(e.target.result, e.total);
-    } catch (error) {
-      console.error(error);
-      alert('Error parsing JSON. Please ensure the file contains valid JSON.');
+  const reader = new FileReader()
+
+  reader.addEventListener('progress', (e) => {
+    if (e.lengthComputable) {
+      const percent = Math.round((e.loaded / e.total) * 100)
+      console.log(`Loading JSON file: ${percent}%`)
     }
-  };
+  })
+
+  reader.addEventListener('load', (e) => {
+    try {
+      console.log('Loaded JSON file with size: ', e.total)
+      handle(e.target.result)
+    } catch (error) {
+      console.error(error)
+      alert('Error parsing JSON. Please ensure the file contains valid JSON.')
+    }
+  })
+
+  reader.readAsText(file, 'UTF-8')
 }
 </script>
 
 <template>
-    <div class="file-input-wrapper">
-        <input type="file" id="fileInput" @change="handleFileChange" hidden>
-        <label for="fileInput" ref="fileLabel">Load JSON</label>
-    </div>
+  <div class="file-input-wrapper">
+    <input type="file" id="fileInput" @change="handleFileChange" hidden />
+    <label for="fileInput" ref="fileLabel">Load JSON</label>
+  </div>
 </template>
 
 <style scoped>
